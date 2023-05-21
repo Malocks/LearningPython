541ef1b72145d732d2077a159f0039a196105cf7
Malocks <m87lange@googlemail.com>
2023-05-21 16:07:19 +0200

    create comments

--- Projekt01/main.py
+++ Projekt01/main.py
@@ -1,5 +1,10 @@
 # Hello world!
+
+#Create a variable with a string
 question = "Whats your name?"
 
+#Create a variable with the input function to ask for a name
 name = input(question)
+
+#Print the Dialog in the console
 print("Hello,",name,"! Welcome to your first practice.")
