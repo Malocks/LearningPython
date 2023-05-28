0d11a0f39e887c4c132f376bb850de5aff233b7d
Malocks <m87lange@googlemail.com>
2023-05-28 15:00:59 +0200

    Delete dir and JSON

--- Projekt01/main.py
+++ Projekt01/main.py
@@ -7,7 +7,9 @@ QUESTION = "Whats your name?: "
 
 # Create a variable with the input function to ask for a name
 name = input(QUESTION)
+
 # Concatenation strings using the format method
 ANSWER_STR = f"Hello, {name}! Welcome to Learn Python."
+
 # Print the Dialog in the console
 print(ANSWER_STR)
