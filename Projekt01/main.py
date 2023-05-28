84070be428ed6c3f605a97c01829d40dac4549b6
Malocks <m87lange@googlemail.com>
2023-05-28 14:46:52 +0200

    String adjustment

--- Projekt01/main.py
+++ Projekt01/main.py
@@ -3,11 +3,11 @@ This module is to ask and response
 """
 
 # Create a variable with a string
-QUESTION = "Whats your name?"
+QUESTION = "Whats your name?: "
 
 # Create a variable with the input function to ask for a name
 name = input(QUESTION)
 # Concatenation strings using the format method
-ANSWER_STR = f"Hello,{name}! Welcome to Learn Python."
+ANSWER_STR = f"Hello, {name}! Welcome to Learn Python."
 # Print the Dialog in the console
 print(ANSWER_STR)
