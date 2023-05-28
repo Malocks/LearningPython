bfb60288c423d4f63c09239056ccc1e4b8dd784a
Malocks <m87lange@googlemail.com>
2023-05-28 10:54:33 +0200

    Create first project

--- Projekt01/main.py
+++ Projekt01/main.py
@@ -1,10 +1,11 @@
 # Hello world!
 
-#Create a variable with a string
+# Create a variable with a string
 question = "Whats your name?"
 
-#Create a variable with the input function to ask for a name
+# Create a variable with the input function to ask for a name
 name = input(question)
-
-#Print the Dialog in the console
-print("Hello,",name,"! Welcome to your first practice.")
+# Concatenation strings using the format method
+answerStr = "Hello,{}! Welcome to Learn Python.".format(name)
+# Print the Dialog in the console
+print(answerStr)
